e85b6f3b460edcd4172c64fab25a5d6fac15c501
Moumen003 <moumenibrahim219@yahoo.com>
2023-11-16 23:00:54 +0200

    git mo2

--- rotr.c
+++ rotr.c
@@ -13,9 +13,7 @@ void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 	if (*head == NULL || (*head)->next == NULL)
 		return;
 	while (copy->next)
-	{
 		copy = copy->next;
-	}
 	copy->next = *head;
 	copy->prev->next = NULL;
 	copy->prev = NULL;
