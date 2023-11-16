fadeb595a5b09ddb2c4ef14b3207f3b1bf2663d5
Moumen003 <moumenibrahim219@yahoo.com>
2023-11-16 23:02:50 +0200

    git mo2

--- main.c
+++ main.c
@@ -36,9 +36,7 @@ int main(int argc, char *argv[])
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
-		{
 			execute(content, &stack, counter, file);
-		}
 		free(content);
 	}
 	free_stack(stack);
