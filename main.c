a4d3ab66266c358aa90464e48b25b45e2db3cd8f
Ma7moudMSA <mahmoud.m.subhy@gmail.com>
2023-11-16 21:26:43 +0200

    l

--- main.c
+++ main.c
@@ -1,45 +1,83 @@
 #include "monty.h"
-Global_Struct_Par Global_Par = {NULL, NULL, NULL, 0};
-/**
-* main - main monty code
-* @Copy_U32_ArgNo: number of arguments
-* @Copy_U32_Env: current environemnt
-* Return: 0 if success
-*/
-int main(int Copy_U32_ArgNo, char *Copy_U32_Env[])
-{
-	unsigned int Local_U32_Counter = 0;
-	FILE *Local_File;
-	stack_t *Local_Stack = NULL;
-	char *Local_U8_Data;
-	size_t Local_Size = 0;
-	ssize_t Local_Ssize_Line = 1;
+stack_t *head = NULL;
 
-	if (Copy_U32_ArgNo != 2)
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	Local_File = fopen(Copy_U32_Env[1], "r");
-	Global_Par.Global_File = Local_File;
-	if (!Local_File)
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", Copy_U32_Env[1]);
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
 		exit(EXIT_FAILURE);
-	}
-	Local_U8_Data = NULL;
-	Local_Ssize_Line = getline(&Local_U8_Data, &Local_Size, Local_File);
-	Global_Par.Global_U8_Value = Local_U8_Data;
-	for (Local_U32_Counter = 1; Local_Ssize_Line > 0; Local_U32_Counter++)
+	if (head == NULL)
 	{
-		Void_Execute(Local_U8_Data, &Local_Stack, Local_U32_Counter, Local_File);
-		free(Local_U8_Data);
-		Local_U8_Data = NULL;
-		Local_Ssize_Line = getline(&Local_U8_Data, &Local_Size, Local_File);
-		Global_Par.Global_U8_Value = Local_U8_Data;
+		head = *new_node;
+		return;
 	}
-	fclose(Local_File);
-	free(Local_U8_Data);
-	Void_FreeStack(Local_Stack);
-return (0);
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
 }
--- monty.h
+++ monty.h
@@ -1,12 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+#define _GNU_SOURCE
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <fcntl.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <stdarg.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -14,62 +16,69 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-/**
- * struct Global_Struct_Par - parameters of stack
- * @Global_U8_Par: value
- * @Global_File: pointer to monty file
- * @Global_U8_Value: line content
- * @Global_U32_Queue: flag change stack <-> queue
- * Description: deines the parameters of the stack
- */
-typedef struct Global_Struct_Par
-{
-	char *Global_U8_Par;
-	FILE *Global_File;
-	char *Global_U8_Value;
-	int Global_U32_Queue;
-}  Global_Struct_Par;
-extern Global_Struct_Par Global_Par;
-void Void_Sub(stack_t **, unsigned int);
-void Void_Mod(stack_t **, unsigned int);
-void Void_AddStack(stack_t **, int);
-void Void_Push(stack_t **, unsigned int);
-void Void_FreeStack(stack_t *);
-void Void_Pall(stack_t **, unsigned int);
-void Void_Pint(stack_t **, unsigned int);
-void Void_Execute(char *, stack_t **, unsigned int, FILE *);
-void Void_Pop(stack_t **, unsigned int);
-void Void_Swap(stack_t **, unsigned int);
-void Void_Add(stack_t **, unsigned int);
-void Void_Nop(stack_t **, unsigned int);
-void Void_Div(stack_t **, unsigned int);
-void Void_Mul(stack_t **, unsigned int);
-void Void_Pchar(stack_t **, unsigned int);
-void Void_Pstr(stack_t **, unsigned int);
-void Void_Rotl(stack_t **, unsigned int);
-void Void_Rotr(stack_t **, unsigned int);
-void Void_AddQueue(stack_t **, int);
-void Void_Stack(stack_t **, unsigned int);
-void Void_Queue(stack_t **, unsigned int);
+
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
+
+/*file operations*/
+void open_file(char *file_name);
+int parse_line(char *buffer, int line_number, int format);
+void read_file(FILE *);
+int len_chars(FILE *);
+void find_func(char *, char *, int, int);
+
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
+
+void call_fun(op_func, char *, char *, int, int);
+
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
+
+/*Math operations with nodes*/
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
+
+/*String operations*/
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
+
+/*Error hanlding*/
+void err(int error_code, ...);
+void more_err(int error_code, ...);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+
 #endif
